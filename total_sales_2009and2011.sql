bac33564d3e5810076922e96c2ddce5b31931ed9
Jennifer Lawson <jenniferlawson000@gmail.com>
2018-09-24 09:19:36 -0500

    Update total_sales_2009and2011.sql

--- total_sales_2009and2011.sql
+++ total_sales_2009and2011.sql
@@ -5,3 +5,10 @@ WHERE InvoiceDate LIKE '%2009%';
 
 SELECT SUM(Total) AS TotalItemsOrdered FROM Invoice
 WHERE InvoiceDate LIKE '%2011%';
+
+SELECT '$' || sum(i.Total) Total,
+strftime('%Y',i.InvoiceDate) InvoiceYear
+FROM Invoice i
+WHERE InvoiceYear = '2011'
+OR InvoiceYear = '2009'
+GROUP BY InvoiceYear;
