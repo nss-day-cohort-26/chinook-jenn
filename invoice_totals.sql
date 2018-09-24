2c96175b17ceb9cefde576fbdac98c5f3d1bce9f
Jennifer Lawson <jenniferlawson000@gmail.com>
2018-09-24 09:17:16 -0500

    Update invoice_totals.sql

--- invoice_totals.sql
+++ invoice_totals.sql
@@ -5,3 +5,10 @@ FROM Invoice i
 JOIN Customer c ON c.CustomerId = i.CustomerId
 JOIN Employee e ON e.EmployeeId = c.SupportRepId
 ORDER by Customer;
+
+SELECT COUNT(i.InvoiceId) NumberOfInvoices,
+strftime('%Y',i.InvoiceDate) as InvoiceYear
+FROM Invoice i
+WHERE InvoiceYear = '2009'
+OR InovoiceYear = '2011'
+GROUP BY InvoiceYear;
